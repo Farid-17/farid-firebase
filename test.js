377eba825061595a972d5c5d446a7dae44872b46
Farid-17 <hammani.farid17@gmail.com>
2023-06-05 02:14:02 +0100

    Remove firebase keys and configs

--- test.js
+++ test.js
@@ -2,13 +2,13 @@
 // import { _get, _first, _save, _delete } from './DB.js';
 
 // setConfigs({
-//     apiKey: 'AIzaSyASYdv5Jl-VlI7S0FOscdAgD4FYjLsOt5Y',
-//     authDomain: 'test-firebase-597f9.firebaseapp.com',
-//     projectId: 'test-firebase-597f9',
-//     storageBucket: 'test-firebase-597f9.appspot.com',
-//     messagingSenderId: '460851519329',
-//     appId: '1:460851519329:web:7700b8a1146ef6fb86ef20',
-//     measurementId: 'G-BZX5EKEQFL',
+//     apiKey: '',
+//     authDomain: '',
+//     projectId: '',
+//     storageBucket: '',
+//     messagingSenderId: '',
+//     appId: '',
+//     measurementId: '',
 // });
 
 // console.log("Saved:", await _save({
